73aba4dba26027f3f5b5da2bcbf9b9032cb5f635
ram1011 <ramece0404@gmail.com>
2022-07-22 16:52:53 +0530

    added scrolling to id functionality

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  styleUrls: ['./user.component.scss'],
 })
 export class UserComponent implements OnInit {
+  constructor(private scroller: ViewportScroller) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  goDown(e: any) {
+    this.scroller.scrollToAnchor(e);
   }
-
 }
